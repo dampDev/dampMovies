d4efd6a92f6cc5cd4d3d1bf864ff2ac2ad6fbf7b
daniel <dmendezp@live.com.mx>
2022-06-30 23:22:02 -0500

    agregamos lista de categorias1, y configuramos Movilfirts

--- src/main.js
+++ src/main.js
@@ -55,4 +55,41 @@ async function getTrandingMoviesPreview(){
 
 }
 
+async function getCategoriesPreview(){
+    const res = await fetch ('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY +'&language=es');
+    
+    const data = await res.json();
+        
+    const categories = data.genres;
+    
+    categories.forEach(category => {
+        const previewCategorisContainer = document.querySelector('#categoriesPreview .categoriesPreview-list');
+
+        const categoryContainer = document.createElement('div');
+       
+
+
+        const categoryTitle= document.createElement('h3');
+        
+       
+
+        categoryTitle.classList.add('category-title');
+        categoryTitle.setAttribute('id','id'+category.id);
+        const categoryTitleText = document.createTextNode(category.name);
+
+        categoryTitle.appendChild(categoryTitleText);
+        categoryContainer.appendChild(categoryTitle);
+        previewCategorisContainer.appendChild(categoryContainer);
+
+
+        
+
+        
+        
+
+    });
+   
+
+}
+getCategoriesPreview();
 getTrandingMoviesPreview();
